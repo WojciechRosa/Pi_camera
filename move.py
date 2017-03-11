620a0eac44729a90bc67d3993a6183c68046307c
WojciechRosa <wojciech.rosa@gmail.com>
2017-03-11 17:55:37 +0100

    Create move.py

---
+++ move.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12,GPIO.OUT)
+pwm=GPIO.PWM(12,50)
+pwm.start(7)
+
+for i in range (40,120):
+        DC=1./18.*(i)+2
+        pwm.ChangeDutyCycle(DC)
+        time.sleep(.05)
+pwm.stop()
+GPIO.cleanup()
