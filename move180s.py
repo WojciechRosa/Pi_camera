4186a37983337a58cc8315a3e053a2e9d06a25a1
root <wojciech.rosa@gmail.com>
2017-03-11 17:27:52 +0100

    <Message>

---
+++ move180s.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12,GPIO.OUT)
+pwm=GPIO.PWM(12,50)
+pwm.start(7)
+
+for i in range (0,180):
+	DC=1./18.*(i)+2
+	pwm.ChangeDutyCycle(DC)
+	time.sleep(.05)
+pwm.stop()
+GPIO.cleanup()
