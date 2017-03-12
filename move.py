fc5c9df58b2e488e8d9423ef1138a3ca8a516b56
WojciechRosa <wojciech.rosa@gmail.com>
2017-03-12 15:54:18 +0100

    wprowadzenia zmian

--- move.py
+++ move.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12,GPIO.OUT)
 pwm=GPIO.PWM(12,50)
 pwm.start(7)
-
+#to jest komentarz
 for i in range (30,110):
         DC=1./18.*(i)+2
         pwm.ChangeDutyCycle(DC)
