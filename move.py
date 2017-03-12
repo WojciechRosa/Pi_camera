23147a68b7ed81c24c056bef73359436d2363e95
WojciechRosa <wojciech.rosa@gmail.com>
2017-03-12 14:49:23 +0100

    a

--- move.py
+++ move.py
@@ -6,7 +6,7 @@ GPIO.setup(12,GPIO.OUT)
 pwm=GPIO.PWM(12,50)
 pwm.start(7)
 
-for i in range (40,41):
+for i in range (40,141):
         DC=1./18.*(i)+2
         pwm.ChangeDutyCycle(DC)
         time.sleep(.05)
